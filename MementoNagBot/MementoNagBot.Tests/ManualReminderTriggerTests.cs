603c4923759c45bda3cd09386a8e16f507186352
James Hughes <james.hughes@codurance.com>
2022-07-12 23:57:17 +0100

    Added test for sending const message to one channel
    Signed-off-by: James Hughes <james.hughes@codurance.com>

--- MementoNagBot/MementoNagBot.Tests/ManualReminderTriggerTests.cs
+++ MementoNagBot/MementoNagBot.Tests/ManualReminderTriggerTests.cs
@@ -1,21 +1,30 @@
+using MementoNagBot.Exceptions;
+using MementoNagBot.Services;
 using MementoNagBot.Triggers;
+using MementoNagBot.Wrappers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Internal;
+using NSubstitute;
 
 
 namespace MementoNagBot.Tests;
 
+// ReSharper disable once ClassNeverInstantiated.Global
 public class ManualReminderTriggerTests
 {
 	public class WhenTheManualReminderTriggerIsCalled
 	{
 		[Fact]
-		public async Task ThenItReturnsHelloWorld()
-		{
-			ManualReminderTrigger trigger = new();
-			HttpRequest req = new DefaultHttpRequest(new DefaultHttpContext());
-			string? res = await trigger.RunAsync(req, null);
-			res.ShouldBe("Hello World!");
-		}
+        public async Task ThenItSendsAMessageToSlackChannel()
+        {
+	        Environment.SetEnvironmentVariable("SLACK_API_TOKEN","12345"); // TODO - Replace with IOptions?
+	        ISlackClient client = Substitute.For<ISlackClient>();
+	        SlackMessageService service = new(client);
+	        ManualReminderTrigger trigger = new(service);
+	        HttpRequest req = new DefaultHttpRequest(new DefaultHttpContext());
+	        await trigger.RunAsync(req, null);
+	        await client.Received(1)
+		        .PostMessageAsync("#botspam", "What is the answer to life, the universe, and everything?");
+        }
 	}
 }
