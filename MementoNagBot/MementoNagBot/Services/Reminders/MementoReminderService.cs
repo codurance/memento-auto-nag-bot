f4002292343d0bb7f681907b21b5f54355e77335
James Hughes <james.hughes@codurance.com>
2022-07-15 18:17:29 +0100

    Added tests and capability to send a general reminder
    Signed-off-by: James Hughes <james.hughes@codurance.com>

--- MementoNagBot/MementoNagBot/Services/Reminders/MementoReminderService.cs
+++ MementoNagBot/MementoNagBot/Services/Reminders/MementoReminderService.cs
@@ -6,9 +6,12 @@ public class MementoReminderService
 {
 	private readonly SlackMessageService _messageService;
 
-	private const string GeneralReminderText = "Hi everyone, it's that time of week again, could you please make sure your Memento is up to date!";
-	private const string MonthEndReminderText = "Hi everyone, tomorrow is month end, could you please make sure that your Memento is up to date and remember to fill it out for tomorrow too!";
-	private const string IndividualReminderTemplate = "Hi {0}, it looks like you've forgotten to fill out your Memento. We know you're busy but we really need it to be kept up to date for billing purposes. So please could you ensure it's updated as soon as possible! If you're having difficulties doing so, please reach out to your manager!";
+	
+	//TODO - Making these public for now, as when I add the translation service, they'll be injected. Just makes this iteration easier.
+	public const string GeneralReminderText = "Hi everyone, it's that time of week again, could you please make sure your Memento is up to date!";
+	public const string MonthEndReminderText = "Hi everyone, tomorrow is month end, could you please make sure that your Memento is up to date and remember to fill it out for tomorrow too!";
+	public const string IndividualReminderTemplate = "Hi {0}, it looks like you've forgotten to fill out your Memento. We know you're busy but we really need it to be kept up to date for billing purposes. So please could you ensure it's updated as soon as possible! If you're having difficulties doing so, please reach out to your manager!";
+	public const string MonthEndIndividualReminderTemplate = "Hi {0}, it looks like you've forgotten to fill out your Memento. We know you're busy but we really need it to be kept up to date for billing purposes. So please could you ensure it's updated as soon as possible! Please also remember to fill out tomorrow as it's month end. If you're having difficulties doing so, please reach out to your manager!";
 
 	public MementoReminderService(SlackMessageService messageService)
 	{
