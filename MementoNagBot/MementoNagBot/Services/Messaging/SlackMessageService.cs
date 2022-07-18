f2d8e8dfad7ed2b821a4afcaa041e990b6f09469
James Hughes <james.hughes@codurance.com>
2022-07-19 00:57:24 +0100

    Added basic logging to the solution
    Signed-off-by: James Hughes <james.hughes@codurance.com>

--- MementoNagBot/MementoNagBot/Services/Messaging/SlackMessageService.cs
+++ MementoNagBot/MementoNagBot/Services/Messaging/SlackMessageService.cs
@@ -1,5 +1,6 @@
 using MementoNagBot.Clients.Slack;
 using MementoNagBot.Models.Options;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using SlackAPI;
 using SlackAPI.RPCMessages;
@@ -10,25 +11,44 @@ public class SlackMessageService
 {
 	private readonly ISlackClient _client;
 	private readonly IOptions<BotOptions> _botOptions;
+	private readonly ILogger<SlackMessageService> _logger;
 
-	public SlackMessageService(ISlackClient client, IOptions<BotOptions> botOptions)
+	public SlackMessageService(ISlackClient client, IOptions<BotOptions> botOptions, ILogger<SlackMessageService> logger)
 	{
 		_client = client;
 		_botOptions = botOptions;
+		_logger = logger;
 	}
 	
 	public virtual async Task SendMessageToBotChannel(string messageText)
 	{
+		_logger.LogDebug("Attempting to send {Message} to {Channel}", messageText, _botOptions.Value.BotChannel);
 		PostMessageResponse res = await _client.PostMessageAsync(_botOptions.Value.BotChannel, messageText);
-		res.AssertOk();
+		if (res.ok)
+		{
+			_logger.LogDebug("Successfully sent message to bot channel!");
+		}
+		else
+		{
+			_logger.LogWarning("Failed to send message to bot channel! Attempting to continue...");
+		}
 	}
 
 	public virtual async Task SendDirectMessageToUser(string userEmail, string message)
 	{
-		UserEmailLookupResponse lookupResponse = await _client.GetUserByEmailAsync(userEmail);
-		lookupResponse.AssertOk();
-		User user = lookupResponse.user;
-		PostMessageResponse res = await _client.PostMessageAsync(user.id, message);
-		res.AssertOk();
+		_logger.LogDebug("Attempting to send {Message} to {UserEmail}", message, userEmail);
+
+		try
+		{
+			UserEmailLookupResponse lookupResponse = await _client.GetUserByEmailAsync(userEmail);
+			lookupResponse.AssertOk();
+			User user = lookupResponse.user;
+			PostMessageResponse res = await _client.PostMessageAsync(user.id, message);
+			res.AssertOk();
+		}
+		catch (Exception)
+		{
+			_logger.LogWarning("Failed to send message to {UserEmail}! Attempting to continue...", userEmail);
+		}
 	}
 }
--- MementoNagBot/MementoNagBot/Triggers/ManualReminderTrigger.cs
+++ MementoNagBot/MementoNagBot/Triggers/ManualReminderTrigger.cs
@@ -10,15 +10,19 @@ namespace MementoNagBot.Triggers;
 public class ManualReminderTrigger
 {
 	private readonly SlackMessageService _messageService;
+	private readonly ILogger<ManualReminderTrigger> _logger;
 
-	public ManualReminderTrigger(SlackMessageService messageService)
+	public ManualReminderTrigger(SlackMessageService messageService, ILogger<ManualReminderTrigger> logger)
 	{
 		_messageService = messageService;
+		_logger = logger;
 	}
 	
 	[FunctionName("ReminderTrigger")]
 	public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
 	{
+		_logger.LogInformation("Function Run with Manual Trigger");
+		
 		// This is rough but it lets us decouple the tests
 		// It'll do until the mementos service is implemented
 		if (req.Query["path"] == "channel")
