84e8d09450d83b701c46669e9697c5143f43dad5
James Hughes <james.hughes@codurance.com>
2022-07-20 13:14:29 +0100

    Added Polly to SlackClientWrapper
    Signed-off-by: James Hughes <james.hughes@codurance.com>

--- MementoNagBot/MementoNagBot/Clients/Slack/SlackClientWrapper.cs
+++ MementoNagBot/MementoNagBot/Clients/Slack/SlackClientWrapper.cs
@@ -1,5 +1,8 @@
 using MementoNagBot.Models.Options;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Polly;
+using Polly.Retry;
 using SlackAPI;
 using SlackAPI.RPCMessages;
 
@@ -7,14 +10,49 @@ namespace MementoNagBot.Clients.Slack;
 
 public class SlackClientWrapper: ISlackClient
 {
+	private readonly ILogger<SlackClientWrapper> _logger;
 	private readonly SlackTaskClient _client;
-	public SlackClientWrapper(IOptions<SlackOptions> slackOptions)
+	private readonly AsyncRetryPolicy<UserEmailLookupResponse> _lookupRetryPolicy;
+	private readonly AsyncRetryPolicy<PostMessageResponse> _messageRetryPolicy;
+
+	private const int NumRetries = 5;
+	
+	
+	public SlackClientWrapper(IOptions<SlackOptions> slackOptions, ILogger<SlackClientWrapper> logger)
 	{
+		_logger = logger;
 		_client = new(slackOptions.Value.SlackApiToken);
+		
+		_lookupRetryPolicy = Policy.HandleResult<UserEmailLookupResponse>(r => !r.ok)
+			.RetryAsync(NumRetries, (_, i) =>
+			{
+				if (i < NumRetries)
+				{
+					_logger.LogWarning("Failed to lookup Slack User ID from Email, will retry!\nRetry count: {RetryCount}", i);
+				}
+				else
+				{
+					_logger.LogError("Can't find Slack User ID from Email... Giving up!");
+				}
+			});
+		
+		_messageRetryPolicy = Policy.HandleResult<PostMessageResponse>(r => !r.ok)
+			.RetryAsync(NumRetries, (_, i) =>
+			{
+				if (i < NumRetries)
+				{
+					_logger.LogWarning("Failed to send message to slack, will retry!\nRetry count: {RetryCount}", i);
+				}
+				else
+				{
+					_logger.LogError("Can't send message to Slack... Giving up!");
+				}
+			});
 	}
 
 
-	public Task<UserEmailLookupResponse> GetUserByEmailAsync(string email) => _client.GetUserByEmailAsync(email);
+	public Task<UserEmailLookupResponse> GetUserByEmailAsync(string email) 
+		=> _lookupRetryPolicy.ExecuteAsync(() => _client.GetUserByEmailAsync(email));
 
 	public Task<PostMessageResponse> PostMessageAsync(
 		string channelId,
@@ -28,7 +66,7 @@ public class SlackClientWrapper: ISlackClient
 		string iconUrl = null!,
 		string iconEmoji = null!,
 		bool asUser = false,
-		string threadTs = null!) =>
-		_client.PostMessageAsync(channelId, text, botName, parse, linkNames, blocks, attachments,
-			unfurlLinks, iconUrl, iconEmoji, asUser, threadTs);
+		string threadTs = null!)
+		=> _messageRetryPolicy.ExecuteAsync(() => 
+			_client.PostMessageAsync(channelId, text, botName, parse, linkNames, blocks, attachments, unfurlLinks, iconUrl, iconEmoji, asUser, threadTs));
 }
