8910a31a1eebdd54db94a65406b5842880c3d327
James Hughes <james.hughes@codurance.com>
2022-07-22 16:10:21 +0100

    Fixed DI and added temp modified trigger time
    Signed-off-by: James Hughes <james.hughes@codurance.com>

--- MementoNagBot/MementoNagBot/Startup.cs
+++ MementoNagBot/MementoNagBot/Startup.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using MementoNagBot.Clients.Memento;
 using MementoNagBot.Clients.Slack;
 using MementoNagBot.Extensions.DependencyInjection;
 using MementoNagBot.Models.Options;
@@ -6,6 +7,7 @@ using MementoNagBot.Providers.DateTimes;
 using MementoNagBot.Services.Gating;
 using MementoNagBot.Services.Messaging;
 using MementoNagBot.Services.Reminders;
+using MementoNagBot.Services.Translation;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -45,7 +47,18 @@ public class Startup: FunctionsStartup
 		
 		builder.Services.AddTransient<ISlackClient, SlackClientWrapper>();
 		builder.Services.AddTransient<IDateProvider, SystemDateProvider>();
+		builder.Services.AddTransient<ITranslatedResourceService, StaticTranslatedResourceService>();
+
+		builder.Services.AddHttpClient<IMementoClient, MementoClient>(c =>
+		{
+			MementoOptions options = config.GetSection("Values:MementoOptions").Get<MementoOptions>();
+			c.BaseAddress = new(options.MementoApiUrl);
+			c.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", options.MementoApiToken);
+		});
 
 		builder.Services.RegisterPollyPolicies();
+
+		
+		
 	}
 }
